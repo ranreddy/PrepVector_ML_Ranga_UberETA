bda57bed45f16aa6bce3ee0df048e5488712495b
Ranga Reddy <163624296+ranreddy@users.noreply.github.com>
2024-07-21 12:19:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import os
 import requests
 
 # Load the trained model
-with open('/content/drive/MyDrive/Maven/model.pkl', 'rb') as file:
+with open('data/model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # Create the Streamlit app
